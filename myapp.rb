f14905704c56114e030ab1822348bb1d04110d40
Syuparn <36665200+Syuparn@users.noreply.github.com>
2019-01-04 15:38:49 +0900

    made ranking page

--- myapp.rb
+++ myapp.rb
@@ -4,9 +4,18 @@ require 'sass'
 require 'haml'
 require 'sinatra/activerecord'
 require './models/tag'
+require './models/article'
 
 get '/' do
   @title = "show tags"
   @tags = Tag.order("num_articles desc").all
   erb :index
+end
+
+get '/ranking/*' do |tag_name|
+  @tag = tag_name
+  @title = "今昔ランキング: #{@tag}"
+  @new_articles = Article.where(tag1: @tag).where(new?: true).all
+  #@old_articles = Article.where(tag1: @tag).where(new?: false).all
+  erb :ranking
 end

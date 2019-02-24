4713ce9afd0675a535aebfeb8a6b47299331b8e0
Syuparn <36665200+Syuparn@users.noreply.github.com>
2019-02-24 21:33:40 +0900

    delete debugprint

--- myapp.rb
+++ myapp.rb
@@ -11,14 +11,6 @@ get '/' do
   erb :index
 end
 
-get '/debug' do
-  @tags = Tag.order("num_articles desc").all
-  @articles = Article.all[1..10]
-  @articles.each { |article|
-    puts "#{article.name}: #{article.num_likes} #{article.tag1} #{article.tag2} #{article.tag3} #{article.tag4} #{article.tag5} #{article.created_at}"
-  }
-end
-
 get '/ranking/*' do |tag_name|
   @tag = tag_name
   @oldest_article = Article.where(tag1: @tag).order("num_likes desc").all[0]

14b1e916616842d516f0fc3e286bc225cbca6c7c
Syuparn <36665200+Syuparn@users.noreply.github.com>
2019-02-27 14:39:41 +0900

    add notation color controller

--- myapp.rb
+++ myapp.rb
@@ -6,6 +6,23 @@ require 'sinatra/activerecord'
 require './models/tag'
 require './models/article'
 
+def diff_year(date)
+  today = Time.now
+  ((today - date) / (365 * 24 * 60 * 60)).to_i
+end
+
+def diff_year_notation_color(diff_year)
+  if diff_year >= 5
+    '#ffdb67'
+  elsif diff_year >= 3
+    '#ffe79a'
+  elsif diff_year >= 1
+    '#fff3cd'
+  else
+    nil
+  end
+end
+
 get '/' do
   @tags = Tag.order("num_articles desc").all
   erb :index
@@ -14,5 +31,7 @@ end
 get '/ranking/*' do |tag_name|
   @tag = tag_name
   @oldest_article = Article.where(tag1: @tag).order("num_likes desc").all[0]
+  @diff_year = diff_year(@oldest_article.created_at)
+  @notation_color = diff_year_notation_color(@diff_year)
   erb :ranking
 end

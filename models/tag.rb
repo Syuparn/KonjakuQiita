3c61f7c769a79a617c97c1fd67c6db6444d84101
Syuparn <36665200+Syuparn@users.noreply.github.com>
2019-03-18 12:15:47 +0900

    move color logic from C to M

--- models/tag.rb
+++ models/tag.rb
@@ -20,4 +20,31 @@ class Tag < ActiveRecord::Base
   def year
     created_at.year
   end
+
+  def badge_color
+    # color gradation depending on diff_year
+    # diff_year==0(now): "success" in Bootstrap
+    # ...(gradation)... diff_year==10(10 years ago): yellow
+    success_color = {r: 40, g: 167, b: 69}
+    old_color = {r: 225, g: 219, b: 103}
+    if diff_year >= 10
+      # use color old directly
+      colorcode(old_color)
+    else
+      gradated_color = success_color.map {|k,v|
+        [k, ((v*(10-diff_year) + old_color[k]*diff_year)/10).to_i]}.to_h
+      colorcode(gradated_color)
+    end
+  end
+
+  private
+  def diff_year
+    today = Time.now
+    ((today - created_at) / (365 * 24 * 60 * 60)).to_i
+  end
+
+  def colorcode(colorhash)
+    # i.e. {r: 16, g: 32, b: 64} -> "#102040"
+    %Q{##{colorhash[:r].to_s(16)}#{colorhash[:g].to_s(16)}#{colorhash[:b].to_s(16)}}
+  end
 end
--- myapp.rb
+++ myapp.rb
@@ -23,25 +23,6 @@ def diff_year_notation_color(diff_year)
   end
 end
 
-def colorcode(colorhash)
-  %Q{##{colorhash[:r].to_s(16)}#{colorhash[:g].to_s(16)}#{colorhash[:b].to_s(16)}}
-end
-
-def diff_year_tag_color(diff_year)
-  # color gradation depending on diff_year
-  # diff_year==0(now): "success" in Bootstrap <-> diff_year==10(10 years ago): yellow
-  success_color = {r: 40, g: 167, b: 69}
-  old_color = {r: 225, g: 219, b: 103}
-  if diff_year >= 10
-    # use color old directly
-    colorcode(old_color)
-  else
-    gradated_color = success_color.map {|k,v|
-      [k, ((v*(10-diff_year) + old_color[k]*diff_year)/10).to_i]}.to_h
-    colorcode(gradated_color)
-  end
-end
-
 tag_sort_order = {
   article: 'num_articles desc',
   name: 'name asc',
@@ -59,7 +40,6 @@ sorted_page_route.each do |sort_key, page_route|
     @tags = Tag.order(tag_sort_order[sort_key]).all
     # FIXME: hide tags untied with articles (tentatively hide tags created_at>=2019)
     @tags = @tags.select {|t| t.created_at.year < 2019}
-    @colors = @tags.map {|tag| diff_year_tag_color(diff_year(tag.created_at))}
     @sort_key = sort_key
     @searched_by = 'all'
     @main_route = '/'
@@ -71,7 +51,6 @@ sorted_page_route.each do |sort_key, page_route|
     @tags = Tag.where('name like ?', "%#{@search_key}%").order(tag_sort_order[sort_key]).all
     # FIXME: hide tags untied with articles (tentatively hide tags created_at>=2019)
     @tags = @tags.select {|t| t.created_at.year < 2019}
-    @colors = @tags.map {|tag| diff_year_tag_color(diff_year(tag.created_at))}
     @sort_key = sort_key
     @searched_by = %Q{contains "#{@search_key}"}
     @main_route = '/search_tags/'

38e6a5b938120811b14aac6627a815554bc3466a
Syuparn <36665200+Syuparn@users.noreply.github.com>
2019-03-18 22:34:22 +0900

    move article logic from V to M

--- models/article.rb
+++ models/article.rb
@@ -12,4 +12,25 @@ end
 
 
 class Article < ActiveRecord::Base
+  def formatted_created_at
+    created_at.strftime('%Y-%m-%d')
+  end
+
+  def formatted_updated_at
+    updated_at.strftime('%Y-%m-%d')
+  end
+
+  def author
+    url.match(%r{https://qiita.com/([^/]+)/})[1]
+  end
+
+  def tags
+    # tag array (which is not nil)
+    [tag1, tag2, tag3, tag4, tag5].compact
+  end
+
+  def tag_urls
+    # replace "#" to %23 not to be treated as sanitized space
+    tags.map {|tag| "/ranking/#{tag.gsub(/#/, '%23')}"}
+  end
 end
